2da331f428200f533c699648188bd0043ed2504a
Pabitra03 <pabitramahakur007@gmail.com>
2024-07-25 20:44:26 +0530

    Initial commit

---
+++ pro01.java
@@ -0,0 +1,6 @@
+// Printing output.
+public class pro01{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ pro02.java
@@ -0,0 +1,11 @@
+// Write a java program that read a celcius degree in a double value from the console then converts it to fahrenheit and display the resilts the formula for the conversion is as follows fahrenheit = (9/5) * celcius + 32.
+import java.util.Scanner;
+public class pro02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a degree in celcius:- ");
+        double celcius = sc.nextDouble();
+        double fahrenheit = (9/5) * celcius + 32;
+        System.out.println("The degree in fahrenheit is:- "+fahrenheit);
+    }
+}
